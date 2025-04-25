587399c27d87a3105b2d0c38bd65d7421d0cd280
Max Sheremet <leheupofall@yandex.ru>
2025-04-25 16:49:52 +0300

    Delegate the logic of comparing the password with the DB and from the form to Spring, passing it only the service with the method for searching for a user by name, make a stub for setting up further encryption logic in the Security config class

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -1,26 +1,31 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.kata.spring.boot_security.demo.security.AuthProviderImpl;
-
-import java.security.AuthProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private AuthProviderImpl authProvider;
+    private final UserDetailsService userDetailsService;
 
     @Autowired
-    public WebSecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
